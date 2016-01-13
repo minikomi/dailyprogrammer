d4768f8504fa975fbc9c42eab3bc0fe618ae7461
minikomi <nerdfunk@gmail.com>
2016-01-13 16:05:46 +0900

    249 - stock quotes

---
+++ 249-stock.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+;; Naive Solution
+
+(define (solve-249 vals (max-pair '(0 0)))
+  (if (>= 2 (length vals)) max-pair
+      (let* ([current (first vals)]
+             [vs      (drop vals 2)]
+             [new-max-pair
+              (for/fold ([current-max-pair max-pair])
+                        ([v vs])
+                (if (> (- v current)
+                       (- (second current-max-pair)
+                          (first current-max-pair)))
+                    (list current v)
+                    current-max-pair))])
+        (solve-249 (rest vals) new-max-pair))))
+
+;; Linear time Solution
+
+(define (solve-249-linear initial-vals)
+  (define (solve-loop max-profit low high vals)
+    (if (>= 2 (length vals)) (list low high)
+        (if (> low (first vals))
+            (solve-loop max-profit (first vals) high (drop vals 2))
+            (if (< max-profit (- (first vals) low))
+                (solve-loop (- (first vals) low) low (first vals) (rest vals))
+                (solve-loop max-profit low high (rest vals))))))
+  (solve-loop -inf.f +inf.f -inf.f initial-vals))
